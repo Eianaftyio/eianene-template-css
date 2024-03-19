16b4694cdcd0c50d14bfdcb52806971946eef4f8
Eianaftyio <163426995+Eianaftyio@users.noreply.github.com>
2024-03-20 04:15:51 +0500

    Initialize bulk-assets-api

---
+++ lib/shopify_api/admin_versions.rb
@@ -0,0 +1,20 @@
+# typed: strict
+# frozen_string_literal: true
+
+module ShopifyAPI
+  module AdminVersions
+    SUPPORTED_ADMIN_VERSIONS = T.let([
+      "unstable",
+      "2022-04",
+      "2022-01",
+      "2021-10",
+      "2021-07",
+      "2021-04",
+    ], T::Array[String])
+
+    LATEST_SUPPORTED_ADMIN_VERSION = T.let("2022-01", String)
+  end
+
+  SUPPORTED_ADMIN_VERSIONS = ShopifyAPI::AdminVersions::SUPPORTED_ADMIN_VERSIONS
+  LATEST_SUPPORTED_ADMIN_VERSION = ShopifyAPI::AdminVersions::LATEST_SUPPORTED_ADMIN_VERSION
+end
---
+++ lib/shopify_api/version.rb
@@ -0,0 +1,6 @@
+# typed: strict
+# frozen_string_literal: true
+
+module ShopifyAPI
+  VERSION = "10.0.3"
+end
