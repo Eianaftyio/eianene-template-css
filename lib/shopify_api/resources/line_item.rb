f01cc69f2a01e1357ae0ccf9003ff11d8370e2a6
Eianaftyio <163426995+Eianaftyio@users.noreply.github.com>
2024-03-20 04:15:36 +0500

    Initialize fix_rubocop_conflicts_2

---
+++ lib/shopify_api/resources/line_item.rb
@@ -0,0 +1,14 @@
+module ShopifyAPI
+  class LineItem < Base
+    class Property < Base
+      def initialize(*args)
+        self.attributes = args[0] || {}
+        self.persisted = args[1] || false
+        super
+      rescue NameError
+        self.attributes = attributes.to_hash
+        self
+      end
+    end
+  end
+end
