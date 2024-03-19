27b273098f62cff96269c8511347752fb2d78ab0
Eianaftyio <163426995+Eianaftyio@users.noreply.github.com>
2024-03-20 04:15:08 +0500

    Initialize headers_in_response

---
+++ Gemfile
@@ -0,0 +1,7 @@
+source "https://rubygems.org"
+
+gemspec
+
+gem 'activeresource-response', :git => 'https://github.com/benjlcox/activeresource-response.git'
+gem "activeresource", "~> 4.0.0"
+
---
+++ lib/active_resource/base_ext.rb
@@ -0,0 +1,23 @@
+module ActiveResource
+  class Base
+    if ActiveResource::VERSION::MAJOR < 4
+      # Backported from ActiveResource master branch
+      def self.headers
+        @headers ||= {}
+
+        if superclass != Object && superclass.headers
+          @headers = superclass.headers.merge(@headers)
+        else
+          @headers
+        end
+      end
+
+      # https://github.com/rails/activeresource/commit/dfef85ce8f653f75673631b2950fcdb0781c313c
+      def self.delete(id, options = {})
+        connection.delete(element_path(id, options), headers)
+      end
+
+      add_response_method :api_response
+    end
+  end
+end
